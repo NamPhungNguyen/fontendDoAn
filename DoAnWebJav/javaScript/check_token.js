f733e135ebf54f4d04e3eb69a45dd29371e89f4a
ngminhoang <108173409+ngminhoang@users.noreply.github.com>
2023-11-23 21:19:23 +0700

    update3

--- DoAnWebJav/javaScript/check_token.js
+++ DoAnWebJav/javaScript/check_token.js
@@ -6,28 +6,34 @@
     var parsedUserInformation = JSON.parse(storedUserInformation);
 
     // Truy cập giá trị của token từ đối tượng
-    var token = parsedUserInformation.token;
+    try {
+        var token = parsedUserInformation.token;
 
-    if (!token) {
-        window.location.href = "home.html";
-    } else {
-        fetch('https://localhost:7156/api/Login/Check', {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Content-Type': 'application/json'
-                //Thêm các header khác nếu cần
-            },
-        })
-            .then(response => {
-                if (response.ok!=true ) {
-                    window.location.href = "home.html";
-                }
+        if (!token) {
+            window.location.href = '../public/home.html';
+        } else {
+            fetch('https://localhost:7156/api/Login/Check', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                    //Thêm các header khác nếu cần
+                },
             })
-            .then(data => {
-            })
-            .catch(error => {
-                console.error("There was a problem with the fetch operation:", error);
-            });
+                .then(response => {
+                    if (response.ok != true) {
+                        window.location.href = '../public/home.html';
+                    }
+                })
+                .then(data => {
+                })
+                .catch(error => {
+                    console.error("There was a problem with the fetch operation:", error);
+                });
+        }
+    }
+
+    catch (error) {
+        window.location.href = '../public/home.html';
     }
 });
