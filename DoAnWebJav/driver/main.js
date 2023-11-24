d8e1d25aa2ee6a9983b524280c81d3359b922cd8
cuongkoi <cuongkoi1406@gmail.com>
2023-11-24 16:22:01 +0700

    làm phần sửa driver

--- DoAnWebJav/driver/main.js
+++ DoAnWebJav/driver/main.js
@@ -13,16 +13,7 @@ fetch(url)
 const tableUsers = document.querySelector('#table-user')
 const renderUsers = (user) => {
     const output = `
-                                        <tr>
-                                            <td>Nguyen Thi No</td>
-                                            <td>0905009999</td>
-                                            <td>notn@gmail.com</td>
-                                            <td>22</td>
-                                            <td> <span>Nam</span> </td>
-                                            <td><a class="btn btn-primary btn-sm">Edit</a> |
-                                                <a class="btn btn-danger btn-sm">Del</a>
-                                            </td>
-                                        </tr>
+                                        
     `;
     tableUsers.insertAdjacentElement('beforeend', output)
 }
