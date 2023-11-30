0a958d9283462da913f17b1231020a698a3e7132
NamPhungNguyen <namhien12082003@gmail.com>
2023-11-30 21:23:53 +0700

    das

--- DoAnWebJav/driver/driver_infor.js
+++ DoAnWebJav/driver/driver_infor.js
@@ -23,6 +23,8 @@ if (parsedUserInformation) {
 }
 
 
+
+
 // api lấy thông tin driver
 const apiInforDriver = `https://localhost:7156/api/Driver/DriverInfor?driverId=${driver_id}`;
 
@@ -35,8 +37,7 @@ const apiGetNumberVehicle = `https://localhost:7156/api/Driver/GetVehicleList?dr
 // api thêm vehicle
 const apiAddVehicle = `https://localhost:7156/api/Driver/CreateVehicle`;
 
-// api sửa xoá vehicle
-const apiDeleteVehicle = `https://localhost:7156/api/Driver/DeleteVehicle?id=1`;
+
 
 // lấy thông tin driver
 fetch(apiInforDriver)
@@ -55,6 +56,7 @@ fetch(apiInforDriver)
     var vehicler = data.vehicle;
     var dataVehicler = vehicler[0].vehicle;
     // lấy thông tin driver
+    // console.log(dataVehicler);
     var driver_name = document.querySelector(".driver_name");
     var image_driver = document.querySelector(".image_driver");
     var password = document.querySelector(".password");
@@ -77,6 +79,7 @@ fetch(apiInforDriver)
     var status_verhicle_false = document.querySelector(
       ".status_verhicle_false"
     );
+
     name_verhicle.value = dataVehicler.vehicleName;
     number_wheel.value = dataVehicler.wheel;
     status_verhicle.value = dataVehicler.status;
@@ -91,37 +94,46 @@ fetch(apiInforDriver)
     var storedUserInformation = localStorage.getItem("userInfo");
     var parsedUserInformation = JSON.parse(storedUserInformation);
 
+
+
+
+
+    // cap nhat lai username khi update
     if (parsedUserInformation) {
-        parsedUserInformation.username = dataDriver.fullName;
-         // Cập nhật lại tên người dùng trong localStorage
-        localStorage.setItem("userInfo", JSON.stringify(parsedUserInformation));
+      parsedUserInformation.username = dataDriver.fullName;
+      // Cập nhật lại tên người dùng trong localStorage
+      localStorage.setItem("userInfo", JSON.stringify(parsedUserInformation));
 
-        // Tạo một container để chứa cả thông tin người dùng và nút đăng xuất
-        var userInfoContainer = document.getElementById("user-info");
+      // Tạo một container để chứa cả thông tin người dùng và nút đăng xuất
+      var userInfoContainer = document.getElementById("user-info");
 
-        var userGreeting = document.createElement("span");
-        userGreeting.textContent = "Chào, " +     dataDriver.fullName;
-        ; // Thông tin về người dùng
+      var userGreeting = document.createElement("span");
+      userGreeting.textContent = "Chào, " + dataDriver.fullName;
+      ; // Thông tin về người dùng
 
-        var logoutButton = document.createElement("button");
-        logoutButton.id = "logoutButton";
-        logoutButton.textContent = "Đăng xuất";
-        logoutButton.addEventListener("click", function () {
-            // Xóa thông tin người dùng từ localStorage và chuyển hướng về trang đăng nhập
-            localStorage.removeItem("userInfo");
-            window.location.href = "../public/login.html";
-        });
+      var logoutButton = document.createElement("button");
+      logoutButton.id = "logoutButton";
+      logoutButton.textContent = "Đăng xuất";
+      logoutButton.addEventListener("click", function () {
+        // Xóa thông tin người dùng từ localStorage và chuyển hướng về trang đăng nhập
+        localStorage.removeItem("userInfo");
+        window.location.href = "../public/login.html";
+      });
 
-        userInfoContainer.innerHTML = ''; // Xóa nội dung hiện tại của userInfoContainer
-        userInfoContainer.appendChild(userGreeting);
-        userInfoContainer.appendChild(logoutButton);
+      userInfoContainer.innerHTML = ''; // Xóa nội dung hiện tại của userInfoContainer
+      userInfoContainer.appendChild(userGreeting);
+      userInfoContainer.appendChild(logoutButton);
     }
-    // lấy thông tin xe
+
   })
   .catch((error) => {
     // Xử lý lỗi ở đây
     console.error("Đã xảy ra lỗi:", error);
   });
+
+
+
+
 
 // chỉnh sửa thông tin driver
 const form = document.querySelector(".form-user");
@@ -170,6 +182,12 @@ form.addEventListener("submit", function (event) {
 
 
 
+
+
+
+
+
+
 fetch(apiGetNumberVehicle)
   .then((response) => {
     if (!response.ok) {
@@ -179,41 +197,135 @@ fetch(apiGetNumberVehicle)
   })
   .then((dataVehicles) => {
     const table = document.getElementById("table-user");
-
-    // Xóa mọi hàng hiện tại trong bảng
-    // while (table.rows.length > 1) {
-    //   table.deleteRow(1);
-    // }
-
+    // console.log(dataVehicles);
     // Lặp qua từng phần tử trong mảng xe
     dataVehicles.forEach((vehicle) => {
       // Tạo một hàng mới trong bảng
-      console.log(vehicle.oviId);
       const row = table.insertRow(-1);
-
       // Thêm các ô dữ liệu vào hàng
-      const nameCell = row.insertCell(0);
-      nameCell.textContent = vehicle.name;
-
-      const desCell = row.insertCell(1);
+      const desCell = row.insertCell(0);
       desCell.textContent = vehicle.description;
 
-      const cargoCell = row.insertCell(2);
+      const cargoCell = row.insertCell(1);
       cargoCell.textContent = vehicle.cargo;
 
-      const fuelEfficiencyCell = row.insertCell(3);
+      const fuelEfficiencyCell = row.insertCell(2);
       fuelEfficiencyCell.textContent = vehicle.fuelEfficiency;
+      fetch(apiInforDriver)
+        .then((response) => {
+          // Kiểm tra xem phản hồi có thành công không (status code 200)
+          if (!response.ok) {
+            throw new Error(`Lỗi mạng: ${response.status}`);
+          }
 
-      const statusCell = row.insertCell(4);
-      statusCell.innerHTML = `<span>${vehicle.status}</span>`;
+          // Chuyển đổi phản hồi sang đối tượng JSON
+          return response.json();
+        })
+        .then((data) => {
+          var vehicler = data.vehicle;
+          console.log(vehicler);
+          // console.log(dataVehicles);
+          vehicler.forEach(item => {
+            const typeVehicle = row.insertCell(3);
+            typeVehicle.textContent = item.vehicle.vehicleName;
+          })
+          // var dataVehicler = vehicler;
 
-      const actionCell = row.insertCell(5);
+        })
+
+
+      // const statusCell = row.insertCell(4);
+      // statusCell.innerHTML = `<span>${vehicle.status}</span>`;
+
+      const actionCell = row.insertCell(3);
+
+      //   <button type="button" class="btn btn-primary btn-add" data-toggle="modal"
+      //   data-target="#exampleModalCenter">
+      //   Tạo xe
+      // </button>
+
 
       // Tạo nút Edit
       const editButton = document.createElement("a");
       editButton.className = "btn btn-primary btn-sm";
       editButton.textContent = "Edit";
       // Thêm sự kiện click cho nút "Edit" (thêm xử lý tương ứng)
+      editButton.addEventListener("click", function () {
+        // Xác định form modal "Edit" dựa trên ID của nó
+        const editModal = document.getElementById("editMyModal");
+
+        // Thêm sự kiện click cho nút "Edit" để hiển thị form modal "Edit"
+        // editButton.addEventListener("click", function () {
+        // Hiển thị form modal "Edit"
+        editModal.style.display = "block"; // Hiển thị modal
+        editModal.classList.add("show"); // Thêm class "show" để hiển thị modal (phụ thuộc vào cách Bootstrap của bạn sử dụng class để điều khiển modal)
+        // handleEditVehicle(vehicle.oviId);
+        const formeditvehicle = document.querySelector('.form-edit-vehicle');
+        formeditvehicle.addEventListener("submit", (e) => {
+          e.preventDefault();
+          // const vehicleName = document.querySelector(".vehicleName");
+          const descriptionVehicle = document.querySelector('.descriptionVehicle').value;
+          const cargoVehicle = document.querySelector('.cargoVehicle').value;
+          const fuelEfficiencyVehicle = document.querySelector('.fuelEfficiencyVehicle').value;
+          const statusVehicle = document.querySelector('.statusVehicle').value;
+          const updatedDataVehicle = {
+            driverId: driver_id,
+            vihcleId: 0,
+            description: descriptionVehicle,
+            status: JSON.parse(statusVehicle),
+            fuelEfficiency: parseFloat(fuelEfficiencyVehicle),
+            cargo: parseFloat(cargoVehicle),
+          };
+          console.log(updatedDataVehicle);
+          const editVehicleApi = `https://localhost:7156/api/Driver/UpdateVehicle?oVIId=${vehicle.oviId}`;
+
+          fetch(editVehicleApi, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedDataVehicle),
+          })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error(`Lỗi mạng: ${response.status}`);
+              }
+              return response.json();
+            })
+            .then((data) => {
+              // Xử lý phản hồi từ server (nếu cần)
+              console.log("Dữ liệu sau khi cập nhật:", data);
+              window.location.reload();
+            })
+            .catch((error) => {
+              // Xử lý lỗi ở đây
+              console.error("Đã xảy ra lỗi:", error);
+            });
+        })
+        // const vehicleName = document.querySelector(".vehicleName");
+        // const descriptionVehicle = document.querySelector('.descriptionVehicle');
+        // const cargoVehicle = document.querySelector('.cargoVehicle');
+        // const fuelEfficiencyVehicle = document.querySelector('.fuelEfficiencyVehicle');
+        // const statusVehicle = document.querySelector('.statusVehicle');
+        const closeForm = document.querySelector('.close-form-edit');
+        closeForm.addEventListener('click', () => {
+          editModal.style.display = "none"; // Hiển thị modal
+          editModal.classList.remove("show"); // Thêm class "show" để hiển thị modal (phụ thuộc vào cách Bootstrap của bạn sử dụng class để điều khiển modal)
+
+        })
+        // });
+
+        // Đưa nút "Edit" vào một phần tử trên trang web của bạn (ví dụ: một div có id là "editButtonContainer")
+        const editButtonContainer = document.getElementById("editButtonContainer");
+        editButtonContainer.appendChild(editButton);
+      });
+
+      // Thêm sự kiện click cho nút "Edit" (thêm xử lý tương ứng)
+
+      // editButton.addEventListener('click', ()=> {
+      //   const formEdit = document.querySelector('.form-edit');
+      //   formEdit.style.display = 'block';
+      // })
 
       // Tạo nút Delete
       const deleteButton = document.createElement("a");
@@ -233,6 +345,20 @@ fetch(apiGetNumberVehicle)
   .catch((error) => {
     console.error("Lỗi khi lấy danh sách xe:", error);
   });
+
+
+
+
+function handleEditVehicle(oviId) {
+
+  const vehicleName = document.querySelector(".vehicleName");
+  const descriptionVehicle = document.querySelector('.descriptionVehicle');
+  const cargoVehicle = document.querySelector('.cargoVehicle');
+  const fuelEfficiencyVehicle = document.querySelector('.fuelEfficiencyVehicle');
+  const statusVehicle = document.querySelector('.statusVehicle');
+}
+
+
 
 function handleDeleteVehicle(oviId) {
   const deleteApiUrl = `https://localhost:7156/api/Driver/DeleteVehicle?id=${oviId}`;
@@ -263,6 +389,7 @@ function handleDeleteVehicle(oviId) {
     });
 }
 
+
 function findRowIndexByoviId(table, oviId) {
   // Tìm vị trí của hàng có chứa thông tin xe với ID tương ứng
   for (let i = 1; i < table.rows.length; i++) {
@@ -278,14 +405,12 @@ function findRowIndexByoviId(table, oviId) {
 
 
 
-//   console.log(driver_id);
 // thêm vehicle
 const btnaddvehicle = document.querySelector(".btn-add-vehicle");
 const formVehicle = document.querySelector(".form-create-vehicle");
 formVehicle.addEventListener("submit", (e) => {
   // Lấy dữ liệu từ các trường nhập liệu trong modal thêm xe
   e.preventDefault();
-  // const nameVehicle = document.querySelector(".name-vehicle").value;
   const descriptionVehicle = document.querySelector(
     ".description-vehicle"
   ).value;
@@ -295,7 +420,6 @@ formVehicle.addEventListener("submit", (e) => {
   const statusVehicle = document.querySelector(".status-vehicle").checked;
   // Tạo đối tượng chứa dữ liệu để gửi lên API
   const newVehicle = {
-    oviId: 25,
     driverId: driver_id,
     vihcleId: 0,
     description: descriptionVehicle,
@@ -329,43 +453,4 @@ formVehicle.addEventListener("submit", (e) => {
       console.error("Đã xảy ra lỗi khi thêm mới xe:", error);
     });
 });
-
-
-
-
-// xoá vehicle
-// const listBtnDelete = document.querySelectorAll(".btnDelete");
-
-
-
-// setTimeout(() => {
-//   const btnDeleteList = document.querySelectorAll(".btnDelete");
-//   // console.log(btnDeleteList);
-// }, 1000);
-// // Gửi request DELETE để xoá phương tiện từ API
-// function deleteVehicle(id) {
-//   const apiDeleteVehicle = `https://localhost:7156/api/Driver/DeleteVehicle?id=${id}`;
-//   fetch(apiDeleteVehicle, {
-//     method: "PUT",
-//   })
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error(`Lỗi mạng: ${response.status}`);
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(
-//         `Phương tiện "${nameVehicle}" với ID ${vehicleId} đã được xóa.`
-//       );
-//       // Cập nhật lại bảng hoặc thực hiện các công việc cần thiết khi xóa thành công.
-//       parentTd.parentElement.remove(); // Xoá hàng khỏi bảng
-//     })
-//     .catch((error) => {
-//       console.error(`Đã xảy ra lỗi khi xóa phương tiện: ${error}`);
-//     });
-// }
-
-
-
 
